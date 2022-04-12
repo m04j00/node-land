3d5973704dc94d127382d7d6860d021886cf3a1a
LEE MINJI <m04j00@gmail.com>
2022-04-12 22:08:13 +0900

    [APP] 홈페이지 구현 #4

--- main.js
+++ main.js
@@ -6,34 +6,67 @@ var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
+
 
     if (pathname === '/') {
-        fs.readFile(`data/${title}`, 'utf-8', (err, description) => {
+        var title = queryData.id;
+        if (title === undefined) {
+            title = 'Welcome';
+            var description = 'Hello, Node.js';
+
             var template = `
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>
-            ${description}
-        </p>
-        </body>
-        </html>
-        `;
+                <!doctype html>
+                <html>
+                <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ol>
+                <h2>${title}</h2>
+                <p>
+                    ${description}
+                </p>
+                </body>
+                </html>
+            `;
             response.writeHead(200);
             response.end(template);
-        });
+        } else {
+            fs.readFile(`data/${title}`, 'utf-8', (err, description) => {
+                var template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                    </head>
+                    <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ol>
+                        <li><a href="/?id=HTML">HTML</a></li>
+                        <li><a href="/?id=CSS">CSS</a></li>
+                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ol>
+                    <h2>${title}</h2>
+                    <p>
+                        ${description}
+                    </p>
+                    </body>
+                    </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        };
+
+
+
     } else {
         response.writeHead(404);
         response.end('Not found');
