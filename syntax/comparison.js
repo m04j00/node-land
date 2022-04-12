2e2a4806886b280cbbe62cb5e5ca03e0ccb8ee3d
LEE MINJI <m04j00@gmail.com>
2022-04-12 21:25:44 +0900

    [JS] 비교 연산자 #4

---
+++ syntax/comparison.js
@@ -0,0 +1,8 @@
+console.log(1 + 1); // 2
+
+console.log(1 == 1); // true
+console.log(1 == 2); // false
+console.log(1 === 1); // true, 이유가 없다면 꼭 세개 사용
+
+console.log(1 > 2); // false
+console.log(1 < 2); // true
