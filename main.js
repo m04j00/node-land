9aadbf82dcab3b570ab8e23a3eae24605a1f087b
Min J <m04j00@gmail.com>
2022-04-13 22:53:59 +0900

    [APP] 글 목록 출력하기 #5

--- main.js
+++ main.js
@@ -7,14 +7,22 @@ var app = http.createServer(function (request, response) {
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
 
-
     if (pathname === '/') {
         var title = queryData.id;
         if (title === undefined) {
-            title = 'Welcome';
-            var description = 'Hello, Node.js';
+            fs.readdir('./data', (err, filelist) => {
+                title = 'Welcome';
+                description = 'Hello Node.js';
 
-            var template = `
+                var list = '<ul>';
+                let i = 0;
+                while (i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i = i + 1;
+                }
+                list += '</ul>';
+
+                var template = `
                 <!doctype html>
                 <html>
                 <head>
@@ -23,11 +31,7 @@ var app = http.createServer(function (request, response) {
                 </head>
                 <body>
                 <h1><a href="/">WEB</a></h1>
-                <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ol>
+                ${list}
                 <h2>${title}</h2>
                 <p>
                     ${description}
@@ -35,37 +39,41 @@ var app = http.createServer(function (request, response) {
                 </body>
                 </html>
             `;
-            response.writeHead(200);
-            response.end(template);
-        } else {
-            fs.readFile(`data/${title}`, 'utf-8', (err, description) => {
-                var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <ol>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                    </ol>
-                    <h2>${title}</h2>
-                    <p>
-                        ${description}
-                    </p>
-                    </body>
-                    </html>
-                `;
                 response.writeHead(200);
                 response.end(template);
+            })
+        } else {
+            fs.readdir('./data', (err, filelist) => {
+                var list = '<ul>';
+                let i = 0;
+                while (i < filelist.length) {
+                    list += `<li><a href="/?id=${filelist[i]}">${filelist[i++]}</a></li>`;
+                }
+                list += '</ul>';
+
+                fs.readFile(`data/${title}`, 'utf-8', (err, description) => {
+                    var template = `
+                        <!doctype html>
+                        <html>
+                        <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                        </head>
+                        <body>
+                        <h1><a href="/">WEB</a></h1>
+                            ${list}
+                        <h2>${title}</h2>
+                        <p>
+                            ${description}
+                        </p>
+                        </body>
+                        </html>
+                    `;
+                    response.writeHead(200);
+                    response.end(template);
+                });
             });
         };
-
-
 
     } else {
         response.writeHead(404);
