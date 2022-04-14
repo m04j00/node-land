807a0075b0d1d008e4e895125102d6b1fe8b5189
Min J <m04j00@gmail.com>
2022-04-14 19:14:55 +0900

    [JS] 함수의 출력 #13

--- syntax/function2.js
+++ syntax/function2.js
@@ -2,7 +2,7 @@ console.log(Math.round(3.6)); // 4
 console.log(Math.round(3.2)); // 3
 
 function sum(a, b){ // parameter
-    console.log(a + b);
+    return a + b;
 }
 
-sum(6, 5); // argument
+console.log(sum(6, 5)); // argument
