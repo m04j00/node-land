76cc48d4a44e4c90c6363e424c495f4b11b1c0a5
Min J <m04j00@gmail.com>
2022-04-18 21:13:18 +0900

    [APP] POST 방식으로 전송된 데이터 받기 #17

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body) {
     return `
@@ -61,7 +62,7 @@ var app = http.createServer(function (request, response) {
             title = 'WEB - create';
             var list = templateList(filelist);
             var template = templateHTML(title, list, 
-                `<form action="http://localhost:3000/process-create" method="post">
+                `<form action="http://localhost:3000/create-process" method="post">
                 <p><input type="text" name="title" placeholder="title"></p>
                 <p>
                     <textarea name="description" placeholder="description"></textarea>
@@ -74,7 +75,20 @@ var app = http.createServer(function (request, response) {
             response.writeHead(200);
             response.end(template);
         })
-    } 
+    } else if(pathname==='/create-process'){
+        var body = '';
+        request.on('data', function(data){
+            body += data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            console.log(post);
+        })
+        response.writeHead(200);
+            response.end('success');
+    }
     else {
         response.writeHead(404);
         response.end('Not found');
