14a6d5cf30cf7111c99ca51cf7eece23b2ef381a
Min J <m04j00@gmail.com>
2022-04-18 21:04:47 +0900

    [APP] 글 생성 UI 만들기 #17

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function templateHTML(title, list, body) {
     <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
     </body>
     </html>
@@ -55,8 +56,26 @@ var app = http.createServer(function (request, response) {
                 });
             });
         };
-
-    } else {
+    } else if(pathname === '/create'){
+        fs.readdir('./data', (err, filelist) => {
+            title = 'WEB - create';
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, 
+                `<form action="http://localhost:3000/process-create" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p>
+                    <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+            </form>`
+                );
+            response.writeHead(200);
+            response.end(template);
+        })
+    } 
+    else {
         response.writeHead(404);
         response.end('Not found');
     }
