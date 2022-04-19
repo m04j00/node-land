ec903418c1c0c8102691100136c986197eb22702
Min J <m04j00@gmail.com>
2022-04-19 19:02:31 +0900

    [APP] 글 삭제 기능 구현 #19

--- main.js
+++ main.js
@@ -51,8 +51,19 @@ var app = http.createServer(function (request, response) {
             fs.readdir('./data', (err, filelist) => {
                 var list = templateList(filelist);
                 fs.readFile(`data/${title}`, 'utf-8', (err, description) => {
-                    var template = templateHTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>
-                    <a href="/update?id=${title}">update</a>`);
+                    var template = templateHTML(
+                        title, 
+                        list, 
+                        `<h2>${title}</h2>${description}`, 
+                        `
+                            <a href="/create">create</a>
+                            <a href="/update?id=${title}">update</a>
+                            <form action="delete-process" method="post">
+                                <input type="hidden" name="id" value="${title}">
+                                <input type="submit" value="delete">
+                            </form>
+                        `
+                        );
                     response.writeHead(200);
                     response.end(template);
                 });
@@ -109,8 +120,11 @@ var app = http.createServer(function (request, response) {
                         <input type="submit">
                     </p>
                 </form>`
-                    , `<a href="/create">create</a>
-                <a href="/update?id=${title}">update</a>`);
+                    , 
+                    `
+                    <a href="/create">create</a>
+                    <a href="/update?id=${title}">update</a>
+                    `);
                 response.writeHead(200);
                 response.end(template);
             });
@@ -132,6 +146,23 @@ var app = http.createServer(function (request, response) {
                     });
                     response.end();
                 })
+            })
+            
+        })
+    } 
+    else if(pathname==='/delete-process'){
+        var body = '';
+        request.on('data', function (data) {
+            body += data;
+        });
+        request.on('end', function () {
+            var post = qs.parse(body);
+            var id = post.id;
+            fs.unlink(`data/${id}`, function(err){
+                response.writeHead(302, {
+                    Location: `/`
+                });
+                response.end();
             })
             
         })
