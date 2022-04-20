23cebdfdca3780387ac317c0ac7c494eb2a842f5
Min J <m04j00@gmail.com>
2022-04-20 14:46:48 +0900

    [JS] 객체의 반복 #21

--- syntax/object.js
+++ syntax/object.js
@@ -1,8 +1,22 @@
 const members = ['mainj', 'joohoney', 'im'];
+
 console.log(members[1]);
 
-const roles = {
-    'rapper' : 'joohoney',
-    'danser' : 'SHOWNU'
+let i = 0;
+while (i < members.length) {
+    console.log('array loop -', members[i]);
+    i += 1;
 }
+
+const roles = {
+    'rapper': 'joohoney',
+    'danser': 'SHOWNU',
+    'singer': 'kihyun'
+}
+
 console.log(roles.rapper);
+console.log(roles['singer']);
+
+for (let name in roles) {
+    console.log('object ->', name, ' value ->', roles[name]);
+}
